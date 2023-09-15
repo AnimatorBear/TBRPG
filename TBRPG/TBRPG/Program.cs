ffbf64dc8e530be5cb6fe69bbe4452213362e447
RudoVerschuren <rpverschuren@st.noorderpoort.nl>
2023-09-15 23:10:16 +0200

    Start Commit Test

--- TBRPG/TBRPG/Program.cs
+++ TBRPG/TBRPG/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "TBRPG";
+            Console.WriteLine("KYS");
+            Console.ReadKey();
         }
     }
 }
